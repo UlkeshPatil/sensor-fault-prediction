46fb6f4def68e8257f5576ba6e4858b38b2aaea2
UlkeshPatil <ulkesh13@gmail.com>
2023-07-17 09:49:39 +0530

    sensor fault ml pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Ulkesh",
+    author_email="ulkesh13@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
